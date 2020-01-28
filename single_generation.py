fcfc04c8e664a635cd4c6b8aae8dda413ac1d859
Mathi Gatti <mathigatti@gmail.com>
2020-01-28 18:22:07 -0300

    single run for news generation and output file

--- single_generation.py
+++ single_generation.py
@@ -39,6 +39,9 @@ parser.add_argument('--print_once', action='store_true',
 parser.add_argument('--topn', type=int, default=0,
                                         help='print top-n candidates during generations; defaults to 0 which is no printing')
 
+parser.add_argument('--input_text', type=str, default='Links https://www.cnn.com/world/09/19/2019/unicorns-found-in-andes-mountains',
+                                        help='Input text')
+
 parser.add_argument('--output_file', type=str, default='output.txt',
                                         help='Output file')
 
@@ -67,9 +70,6 @@ idx2word = np.array(vocab)
 # the model is trained with a seq_length of 512
 # so, any value <= 512 should work
 seq_length = min(args.generate_num, 256)
-
-
-
 
 # the dimension of the transformer
 embedding_dim = 1280
@@ -112,7 +112,6 @@ embedded = tied_embedding_softmax(tokens, embed=True)
 # for some odd reason, TPUs don't play well with specifying the arguments of the Encoder() function
 # so you have to leave them at their defaults
 transformed = transformer.Encoder()(embedded, training=False)
-
 
 # pass the activations from our tiedsoftmax class
 # this time with embed=False denoting that we are doing the softmax operation
@@ -166,134 +165,135 @@ bpe = fastBPE.fastBPE('codes', 'vocab')
 
 temperature = args.temperature
 nucleusprob = args.nucleus
+prompt = args.input_text 
 penalty = args.penalty
 topk = args.topk
 
-while True:
-    prompt = raw_input('ENTER PROMPT: ') if not use_py3 else input('ENTER PROMPT: ')
-    prompt = prompt.split('\\n') # split on newlines if provided
+prompt = raw_input('ENTER PROMPT: ') if not use_py3 else input('ENTER PROMPT: ')
+prompt = prompt.split('\\n') # split on newlines if provided
 
-    # tokenize provided prompt
-    split_prompt = ' \n '.join(bpe.apply(prompt))
-    split_prompt = split_prompt.split(' ')
+# tokenize provided prompt
+split_prompt = ' \n '.join(bpe.apply(prompt))
+split_prompt = split_prompt.split(' ')
 
-    if not any(split_prompt[0] == x for x in CONTROL_CODES.keys()):
-        print("WARNING! You are not starting your generation from a control code so you won't get good results")
-    text = [word2idx[i] for i in split_prompt]
+if not any(split_prompt[0] == x for x in CONTROL_CODES.keys()):
+    print("WARNING! You are not starting your generation from a control code so you won't get good results")
+text = [word2idx[i] for i in split_prompt]
 
-    # pad with 0s and create a mini-batch of 2 (arbitrary, for ease of code)
-    padded_text = text + [0] * (args.generate_num - len(text))
-    tokens_generated = np.tile(padded_text, (1,1))
-    try:
-        for token in range(len(text)-1, args.generate_num-1):
-          # get the logits from the prediction function
-          # the logic here is a bit convoluted because we are allowing generation past 512 tokens
-          # this is done by sliding the window over (past 512 tokens) and continuing prediction
-          # I'm sure this can be simplified (TODO)
-          if token <= seq_length:
-            prompt_logits = predict_fn({'input_1':tokens_generated[:, :seq_length]})['tied_embedding_softmax'].squeeze() / (temperature if temperature>0 else 1.)
-            _token = token if token < seq_length else -1
-          else:
-            _token = -1
-            end = token + 1
-            start = token - seq_length + 2
-            prompt_logits = predict_fn({'input_1':np.hstack((tokens_generated[:,0:1], tokens_generated[:,start:end]))})['tied_embedding_softmax'].squeeze() / (temperature if temperature>0 else 1.)
-
-
-          # if penalty (for repetition) is non-zero,
-          # discount the logits from already generated tokens
-          if penalty>0:
-              penalized_so_far = set()
-              for _ in range(token+1):
-                 generated_token = tokens_generated[0][_]
-                 # don't penalize newlines
-                 # you could also choose not to penalize frequent words
-                 # (which incidentally are sorted in the vocab file)
-                 # but I don't do that
-                 # if it prints too many new lines instead of continuing generating text,
-                 # you might want to comment this out
-                 if idx2word[generated_token] == '\n':
-                     continue
-                 if generated_token in penalized_so_far:
-                     continue
-                 penalized_so_far.add(generated_token)
-                 prompt_logits[_token][generated_token] /= penalty
-
-          # disallow some tokens
-          prompt_logits[_token][word2idx['<unk>']] = -1e8
-
-          # sometimes, when generating from reddit,
-          # it tries to generate the Score (reddit Karma) immediately after generating the Title:
-          # to disallow this, we can just prevent it from generating Score
-          prompt_logits[_token][word2idx['Sco@@']] = -1e8
+# pad with 0s and create a mini-batch of 2 (arbitrary, for ease of code)
+padded_text = text + [0] * (args.generate_num - len(text))
+tokens_generated = np.tile(padded_text, (1,1))
+try:
+    for token in range(len(text)-1, args.generate_num-1):
+      # get the logits from the prediction function
+      # the logic here is a bit convoluted because we are allowing generation past 512 tokens
+      # this is done by sliding the window over (past 512 tokens) and continuing prediction
+      # I'm sure this can be simplified (TODO)
+      if token <= seq_length:
+        prompt_logits = predict_fn({'input_1':tokens_generated[:, :seq_length]})['tied_embedding_softmax'].squeeze() / (temperature if temperature>0 else 1.)
+        _token = token if token < seq_length else -1
+      else:
+        _token = -1
+        end = token + 1
+        start = token - seq_length + 2
+        prompt_logits = predict_fn({'input_1':np.hstack((tokens_generated[:,0:1], tokens_generated[:,start:end]))})['tied_embedding_softmax'].squeeze() / (temperature if temperature>0 else 1.)
 
 
-          # compute probabilities from logits
-          prompt_probs = np.exp(prompt_logits[_token])
-          prompt_probs = prompt_probs / sum(prompt_probs)
-          pruned_list = np.argsort(prompt_probs)[::-1]
-          # if you are using nucleus prob, then compute the nucleus probability size
-          if nucleusprob > 0.:
-            minimum_topk = 1
-            nucleus = max(np.where(np.cumsum(np.sort(prompt_probs)[::-1])>nucleusprob)[0][0], minimum_topk)
-          elif topk > 0:
-            # we are over-loading notation here
-            # if you choose to specify a topk instead of a nucleus,
-            # we will hardcode the nucleus to be just that
-            nucleus = topk
-          else:
-            # if you specify neither nucleus or topk,
-            # then we will use the whole list
-            nucleus = len(pruned_list)
-            
-          pruned_list = pruned_list[:nucleus]  
-          # if you want to disallow more complex tokens, you can do so here
-          # for instance, if you want to disallow anything with the phrase `http`,
-          # you can delete theme from the pruned_list
-          # you can comment this out, I'm keeping it in for demonstration purpose
-          tokens_to_disallow = []
-          for _ in range(len(pruned_list)):
-              if 'http' in idx2word[pruned_list[_]]:
-                  tokens_to_disallow.append(_)
-          pruned_list = np.delete(pruned_list, tokens_to_disallow)
+      # if penalty (for repetition) is non-zero,
+      # discount the logits from already generated tokens
+      if penalty>0:
+          penalized_so_far = set()
+          for _ in range(token+1):
+             generated_token = tokens_generated[0][_]
+             # don't penalize newlines
+             # you could also choose not to penalize frequent words
+             # (which incidentally are sorted in the vocab file)
+             # but I don't do that
+             # if it prints too many new lines instead of continuing generating text,
+             # you might want to comment this out
+             if idx2word[generated_token] == '\n':
+                 continue
+             if generated_token in penalized_so_far:
+                 continue
+             penalized_so_far.add(generated_token)
+             prompt_logits[_token][generated_token] /= penalty
 
-          if args.topn > 0 :
-            print('TOPN :: top-n alternatives:', [idx2word[_] for _ in pruned_list[:args.topn]])
+      # disallow some tokens
+      prompt_logits[_token][word2idx['<unk>']] = -1e8
 
-          # if temperature is 0
-          # just pick the first (most probable) token
-          if temperature==0:
-              idx = pruned_list[0]
-          else:
-            # else,
-            # sample from the pruned_list with the logits
-            chosen_idx = int(tf.random.categorical(np.expand_dims(prompt_logits[_token][pruned_list],0), num_samples=1).numpy())
-            idx = pruned_list[chosen_idx]
-
-          if args.topn > 0 :
-            print('TOPN :: chosen word:', idx2word[idx])
-
-          # assign the token for generation
-          tokens_generated[0][token+1] = idx
-
-          # clear screen if you want to
-          # os.system("clear")
+      # sometimes, when generating from reddit,
+      # it tries to generate the Score (reddit Karma) immediately after generating the Title:
+      # to disallow this, we can just prevent it from generating Score
+      prompt_logits[_token][word2idx['Sco@@']] = -1e8
 
 
-          tokens_generated_so_far = ' '.join([idx2word[c] for c in tokens_generated[0].squeeze()[:token+2]])
-          tokens_generated_so_far = re.sub('(@@ )', '', string=tokens_generated_so_far)
-          tokens_generated_so_far = re.sub('(@@ ?$)', '', string=tokens_generated_so_far)              
+      # compute probabilities from logits
+      prompt_probs = np.exp(prompt_logits[_token])
+      prompt_probs = prompt_probs / sum(prompt_probs)
+      pruned_list = np.argsort(prompt_probs)[::-1]
+      # if you are using nucleus prob, then compute the nucleus probability size
+      if nucleusprob > 0.:
+        minimum_topk = 1
+        nucleus = max(np.where(np.cumsum(np.sort(prompt_probs)[::-1])>nucleusprob)[0][0], minimum_topk)
+      elif topk > 0:
+        # we are over-loading notation here
+        # if you choose to specify a topk instead of a nucleus,
+        # we will hardcode the nucleus to be just that
+        nucleus = topk
+      else:
+        # if you specify neither nucleus or topk,
+        # then we will use the whole list
+        nucleus = len(pruned_list)
+        
+      pruned_list = pruned_list[:nucleus]  
+      # if you want to disallow more complex tokens, you can do so here
+      # for instance, if you want to disallow anything with the phrase `http`,
+      # you can delete theme from the pruned_list
+      # you can comment this out, I'm keeping it in for demonstration purpose
+      tokens_to_disallow = []
+      for _ in range(len(pruned_list)):
+          if 'http' in idx2word[pruned_list[_]]:
+              tokens_to_disallow.append(_)
+      pruned_list = np.delete(pruned_list, tokens_to_disallow)
 
-          if not args.print_once:
-            print('---------------------------------------')
-            print(tokens_generated_so_far)
-            print()
-        print('---------------------------------------')            
+      if args.topn > 0 :
+        print('TOPN :: top-n alternatives:', [idx2word[_] for _ in pruned_list[:args.topn]])
+
+      # if temperature is 0
+      # just pick the first (most probable) token
+      if temperature==0:
+          idx = pruned_list[0]
+      else:
+        # else,
+        # sample from the pruned_list with the logits
+        chosen_idx = int(tf.random.categorical(np.expand_dims(prompt_logits[_token][pruned_list],0), num_samples=1).numpy())
+        idx = pruned_list[chosen_idx]
+
+      if args.topn > 0 :
+        print('TOPN :: chosen word:', idx2word[idx])
+
+      # assign the token for generation
+      tokens_generated[0][token+1] = idx
+
+      # clear screen if you want to
+      # os.system("clear")
+
+
+      tokens_generated_so_far = ' '.join([idx2word[c] for c in tokens_generated[0].squeeze()[:token+2]])
+      tokens_generated_so_far = re.sub('(@@ )', '', string=tokens_generated_so_far)
+      tokens_generated_so_far = re.sub('(@@ ?$)', '', string=tokens_generated_so_far)              
+
+      if not args.print_once:
+        print('---------------------------------------')
         print(tokens_generated_so_far)
         print()
-        with open(output_file,'w') as f:
-          f.write(tokens_generated_so_far)
+    print('---------------------------------------')
+    print(tokens_generated_so_far)
+    print()
+    with open(output_file,'w') as f:
+      f.write(tokens_generated_so_far)
 
-    except KeyboardInterrupt: #Exception as e:
-        print('Continuing')
-            
+
+except KeyboardInterrupt: #Exception as e:
+    pass
+        
